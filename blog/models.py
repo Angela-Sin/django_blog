f09bd07306525922e1543fd5519656cf865489ab
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-12-10 18:04:03 +0000

    add challrnge bug

--- blog/models.py
+++ blog/models.py
@@ -26,6 +26,7 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
+    challenge = models.SlugField()
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
