e53d78dc4919ed3a37f45b5ba1ecbdffa925c761
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-12-14 18:10:10 +0000

    add about file can't do migration

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('', views.EventsList.as_view(), name='home'),
+    path("<int:event_id>/", views.event_detail,
+         name="event_detail") 
 ]
